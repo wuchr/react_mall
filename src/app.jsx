e7dd0a090c4b0d4f95ce02f213b2fb64e2eb32a8
wuchr <1569528869@qq.com>
2018-11-27 22:12:18 +0800

    添加登录页面

--- src/app.jsx
+++ src/app.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import  ReactDOM from 'react-dom';
 import Home from 'page/home/index.jsx';
 import Layout from 'component/layout/index.jsx';
+import Login from 'page/login/index.jsx';
 import {  BrowserRouter as Router, Route, Switch,Redirect} from 'react-router-dom';
 class App extends React.Component{
     render() {
         return (
             <Router>
-                <Layout>
-                    <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Route  path="/product" component={Home}/>
-                        <Route  path="/product-category" component={Home}/>
-                    </Switch>
-                </Layout>
+                <Switch>
+                    <Route  path="/login" component={Login}/>
+                    <Route  path="/" render={ props => (
+                        <Layout>
+                            <Switch>
+                                <Route exact path="/" component={Home}/>
+                                <Route  path="/product" component={Home}/>
+                                <Route  path="/product-category" component={Home}/>
+                            </Switch>
+                        </Layout>
+                    )}/>
+                </Switch>
+
             </Router>
         );
     }
